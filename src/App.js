6e296f72bf9773b3206984afe9375548ca8152a5
Ogun Akar <ogun.akar@csadmins-MacBook-Pro.local>
2021-12-29 10:31:53 +0300

    add test for checkbox disabling button

--- src/App.js
+++ src/App.js
@@ -6,14 +6,23 @@ function App() {
   const [buttonColor, setButtonColor] = useState("red");
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
 
+  const [disabled, setDisabled] = useState(false);
+
   return (
     <div>
       <button
         onClick={() => setButtonColor(newButtonColor)}
         style={{ backgroundColor: buttonColor }}
+        disabled={disabled}
       >
         Change to {newButtonColor}
       </button>
+      <input
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+        type="checkbox"
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -10,3 +10,21 @@ test("button has correct initial color", () => {
   expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
   expect(colorButton.textContent).toBe("Change to red");
 });
+
+test("clicking checkbox disables button", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  const checkbox = screen.getByRole("checkbox");
+
+  expect(colorButton).toBeEnabled();
+
+  expect(checkbox).not.toBeChecked();
+
+  fireEvent.click(checkbox);
+  expect(checkbox).toBeChecked();
+  expect(colorButton).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(checkbox).not.toBeChecked();
+  expect(colorButton).toBeEnabled();
+});
